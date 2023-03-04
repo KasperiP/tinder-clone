eca80a1df44bb7b8603d49db18d51b0e6357fa7f
kassq <kasperi.pohtine@gmail.com>
2023-03-04 20:34:27 +0200

    feat: messages & profile screen texts

--- pages/Messages.tsx
+++ pages/Messages.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Text from '../components/Text/Text';
 
 const MessagesScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>Messages Screen</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title} variant="bold">
+        Messages
+      </Text>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 30,
+    paddingTop: 10,
+    paddingBottom: 0,
+  },
+  title: {
+    fontSize: 34,
+    marginBottom: 15,
+  },
+});
 
 export default MessagesScreen;
